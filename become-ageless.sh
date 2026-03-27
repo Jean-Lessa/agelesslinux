0c98895f8e29b5186c4fd561d20e2b26102ed0c0
Jean-Lessa <75625278+Jean-Lessa@users.noreply.github.com>
2026-03-27 00:52:24 -0300

    00-header: level up the social commentary

--- become-ageless.sh
+++ become-ageless.sh
@@ -7,36 +7,54 @@
 # ============================================================================
 # [EN-US]
 #
-# This script converts your existing Linux installation into
-# Ageless Linux, a California-regulated operating system.
+# This script converts your existing Linux installation into Ageless Linux,
+# an operating system regulated by digital surveillance laws sanctioned from,
+# by and for states, counties and countries like California and Brazil.
 #
-# By running this script, the person or entity who controls this
-# device becomes an "operating system provider" as defined by
-# California Civil Code § 1798.500(g), because they now "control
-# the operating system software on a general purpose computing device."
+# By running this script, the person or entity who controls this device becomes:
+#
+# * An "operating system provider" as defined by California Civil Code
+#   § 1798.500(g), part of the California Digital Age Assurance Act (AB 1043,
+#   Chapter 675, Statues of 2025), because they now "control the operating
+#   system software on a general purpose computing device"
+# * Susceptible to the application of Brazilian Law nº 15.211/2025
+#   (known as "Lei Felca"), in force since March 17th 2026, because their
+#   operating system is now bound to item VII of Article 2nd of said law -
+#   this includes any adjacent laws referenced by it, e.g. Statute of the
+#   Child and Adolescent (L8069/1990), Civil Rights Framework for the Internet
+#   (L12692/2014), and the General Data Protection Law (L13709/2018, aka "LGPD",
+#   aka "Brazilian GDPR")
 #
 # Ageless Linux does not collect, store, transmit, or even think about
-# the age of any user, in full and knowing noncompliance with the
-# California Digital Age Assurance Act (AB 1043, Chapter 675,
-# Statutes of 2025).
+# the age of any user, in full and knowing noncompliance with all of the
+# laws/acts/etc. stated above.
+#
 # ============================================================================
 # [PT-BR]
 #
 # Este script converte sua instalação Linux existente no Ageless Linux,
-# um sistema operacional definido pelo inciso VII do Art. 2º da
-# Lei Brasileira nº 15.211, de 17 de Setembro de 2025 (vulgo "Lei Felca").
+# um sistema operacional regulado por leis de vigilância digital sancionadas
+# de, por e para estados, condados e países como Califórnia e Brasil.
 #
 # Ao rodar este script, a pessoa ou entidade que controla este dispositivo
-# se torna suscetível à aplicação desta lei, incluso todas as leis
-# adjacentes à mesma:
+# se torna:
 #
-# * Estatuto da Criança e do Adolescente (L8069/1990)
-# * Marco Civil da Internet (L12692/2014)
-# * Lei Geral de Proteção de Dados (L13709/2018)
+# * Um "provedor de sistema operacional", como definido pelo Código Civil
+#   da Califórnia § 1798.500(g), parte da Lei de Garantia da Idade Digital
+#   da Califórnia (AB 1043, Capítulo 675, Estatutos de 2025), porque ela agora
+#   agora "controla o software do sistema operacional em um dispositivo de
+#   computação de uso geral"
+# * Suscetível à aplicação da Lei Braslieira nº 15.211/2025 (vulgo "Lei Felca"),
+#   em vigor desde 17 de Março de 2026, porque seu sistema operacional agora
+#   se enquadra no inciso VII do Art. 2º de tal lei - isto inclui quaisquer
+#   outras leis adjacentes referenciadas pela mesma, tais como o Estatuto da
+#   Criança e do Adolescente (L8069/1990), o Marco Civil da Internet
+#   (L12692/2014), e a Lei Geral de Proteção de Dados (L13709/2018, vulgo
+#   "LGPD", vulgo "GDPR Tupiniquim")
 #
 # Ageless Linux não coleta, armazena, transmite ou sequer pensa sobre
 # a idade de qualquer usuário, em completa e proposital não-conformidade
-# com a Lei Brasileira nº 15.211/2025, em vigor desde 17 de Março de 2026.
+# com todas as leis/atos/etc. descritos acima.
 # ============================================================================
 
 
